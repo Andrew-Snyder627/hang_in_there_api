37e602ad7d84f6437795a9e3f8a4cf4d4e22a321
Calvin Suiter <cdsuiter@yahoo.com>
2025-04-09 10:16:46 -0400

    created and cleared tests for create a poster

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -27,31 +27,75 @@ describe "Posters API", type: :request do
 
     expect(response).to be_successful
 
-    posters = JSON.parse(response.body, symbolize_names: true)
+    json  = JSON.parse(response.body, symbolize_names: true)
+    posters = json[:data]
 
     expect(posters.count).to eq(3)
 
-    posters.each do |poster|
-      expect(poster).to have_key(:id)
-      expect(poster[:id]).to be_an(Integer)
+  #   posters.each do |poster|
+  #     expect(poster).to have_key(:id)
+  #     expect(poster[:id].to_i).to be_an(Integer)
 
-      expect(poster).to have_key(:name)
-      expect(poster[:name]).to be_a(String)
+  #     expect(poster).to have_key(:name)
+  #     expect(poster[:name]).to be_a(String)
 
-      expect(poster).to have_key(:description)
-      expect(poster[:description]).to be_a(String)
+  #     expect(poster).to have_key(:description)
+  #     expect(poster[:description]).to be_a(String)
 
-      expect(poster).to have_key(:price)
-      expect(poster[:price]).to be_a(Float)
+  #     expect(poster).to have_key(:price)
+  #     expect(poster[:price]).to be_a(Float)
 
-      expect(poster).to have_key(:year)
-      expect(poster[:year]).to be_a(Integer)
+  #     expect(poster).to have_key(:year)
+  #     expect(poster[:year]).to be_a(Integer)
 
-      expect(poster).to have_key(:vintage)
-      expect(poster[:vintage]).to be_a(Boolean)
+  #     expect(poster).to have_key(:vintage)
+  #     expect(poster[:vintage]).to be_a(Boolean)
 
-      expect(poster).to have_key(:img_url)
-      expect(poster[:img_url]).to be_a(String)
-    end
+  #     expect(poster).to have_key(:img_url)
+  #     expect(poster[:img_url]).to be_a(String)
+  #   end
+  # end
+
+  posters.each do |poster|
+    expect(poster).to have_key(:id)
+    expect(poster[:id].to_i).to be_an(Integer) # string in the serializer
+
+    expect(poster).to have_key(:type)
+    expect(poster[:type]).to eq("poster")
+
+    expect(poster).to have_key(:attributes)
+
+    attrs = poster[:attributes]
+
+    expect(attrs[:name]).to be_a(String)
+    expect(attrs[:description]).to be_a(String)
+    expect(attrs[:price]).to be_a(Float)
+    expect(attrs[:year]).to be_a(Integer)
+    expect(attrs[:vintage]).to be_in([true, false])
+    expect(attrs[:img_url]).to be_a(String)
+  end
+end
+
+  it "can create a new poster" do
+    poster_params = {
+      "name": "DEFEAT",
+      "description": "It's too late to start now.",
+      "price": 35.00,
+      "year": 2023,
+      "vintage": false,
+      "img_url":  "https://unsplash.com/photos/brown-brick-building-with-red-car-parked-on-the-side-mMV6Y0ExyIk"
+    }
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post "/api/v1/posters", headers: headers, params: JSON.generate(poster: poster_params)
+    created_poster = Poster.last
+
+    expect(response).to be_successful
+    expect(created_poster.name).to eq(poster_params[:name])
+    expect(created_poster.description).to eq(poster_params[:description])
+    expect(created_poster.price).to eq(poster_params[:price])
+    expect(created_poster.year).to eq(poster_params[:year])
+    expect(created_poster.vintage).to eq(poster_params[:vintage])
+    expect(created_poster.img_url).to eq(poster_params[:img_url])
   end
 end
